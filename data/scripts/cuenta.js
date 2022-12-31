bc0a13bcc7cc36f239d6f349c97a8ddb2428c3cb
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-12-31 17:39:19 -0300

    filtrar

--- data/scripts/cuenta.js
+++ data/scripts/cuenta.js
@@ -1,32 +1,40 @@
-let cuenta = {
-	nombre: null,
+let cuenta = { //Setup de la cuenta
+	nombre: localStorage.getItem("lnombre"),
 	descripcion: null,
-	genero: null,
+	genero: localStorage.getItem("lgenero"),
 	cadeado: true
 }
 
-function load(){
-	if (cuenta.cadeado == true){
+function load(){ //Load de la pagina
+	if(cuenta.nombre != null){ //Si la cuenta ya tienes nombre(luego ya se creó), cambiar el valor de lo cadeado
+		cuenta.cadeado = false;
+	}
+	
+	if (cuenta.cadeado == true){//Retorno si no tienes cuenta
 		aler = document.createElement('section');
 		aler.setAttribute('id', 'aler');
 		aler.classList.add('section');
 		aler.innerHTML = `
-			<h1>Não tens conta.</h1>
-			<button onclick="criar(aler)">Criar</button>
+			<h1>No tienes cuenta.</h1>
+			<button onclick="criar(aler)">Crear</button>
 		`;
 		document.getElementById('main').appendChild(aler);
-	}else if(cuenta.cadeado == false){
+	}else if(cuenta.cadeado == false){//Retorno si tienes cuenta
 		menu = document.createElement('section');
 		menu.setAttribute('id', 'menu');
 		menu.classList.add('section');
 		menu.innerHTML = `
-			<h1>Teste.</h1>
+			<h1>Detectamos un(a) usuário(a)!</h1>
+			<h2>${cuenta.nombre}</h2>
+			<p>Qué pretendes hacer?</p>
+			<input type="button" value="logar" id="logar" class="logar" />
+			<button onclick="deletar(menu)" id="deletar" class="deletar">Deletar Cuenta</button>
 		`;
 		document.getElementById('main').appendChild(menu);
 	}
 }
 
-function criar(group){
+function criar(group){//Pagina de crar la cuenta
 		group.remove();
 		let forma = document.createElement('form');
 		forma.setAttribute('id', 'form');
@@ -55,7 +63,7 @@ function criar(group){
 	document.getElementById('main').appendChild(forma);
 }
 
-function enviar(){
+function enviar(){//Enviar los datos para que pueden ser tratados depues
 	let nnombre = document.getElementById('nombre').value;
 	let ngenero = 
 	[
@@ -68,20 +76,36 @@ function enviar(){
 		window.alert('Por favor, defina un nick.');
 	}else{
 		if(ngenero[0].checked){
-			console.log(`${nombre.value}, hombre`);
-			filtrar(ngenero);
+			ngenero = 'Hombre';
+			filtrar(ngenero, nnombre);
 		}else if(ngenero[1].checked){
-			console.log(`${nombre.value}, mujer`);
-			filtrar(ngenero);
+			ngenero = 'Mujer';
+			filtrar(ngenero, nnombre);
 		}else if(ngenero[2].checked){
-			console.log(`${nombre.value}, otro`);
-			filtrar(ngenero);
+			ngenero = 'Otro';
+			filtrar(ngenero, nnombre);
 		}else{
 			window.alert('Esqueció de configurar el genero.');
 		}
 	}
 }
 
-function filtrar(ngenero){
+function filtrar(ngenero, nnombre){ //Filtrar los datos(descargalos en su navegador)
 	document.getElementById('form').remove();
+	cuenta.nombre = nnombre;
+	cuenta.genero = ngenero;
+	localStorage.setItem('lnombre', cuenta.nombre);
+	localStorage.setItem('lgenero', cuenta.genero);
+	console.log(`${localStorage.getItem("lnombre")}, ${localStorage.getItem("lgenero")}`);
+	load();
+}
+
+function deletar(group){//Remover los datos(incluso de su navegador)
+	group.remove();
+	cuenta.cadeado = true;
+	cuenta.nombre = null;
+	cuenta.genero = null;
+	localStorage.removeItem("lnombre");
+	localStorage.removeItem("lgenero");
+	load();
 }
